7eca6bed5a7c83ec94bac4b7363d262b5e5c611b
Vanessa Castro <vanessacastrol16@gmail.com>
2021-03-28 16:38:13 -0700

    added styling and finished project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import data from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [superhero, setSuperhero] = useState(data);
+	const [index, setIndex] = useState(0);
+
+	useEffect(() => {
+		const lastIndex = superhero.length - 1;
+		if(index < 0) {
+			setIndex(lastIndex);
+		}
+		if(index > lastIndex) {
+			setIndex(0);
+		}
+	}, [index, superhero]);
+
+	useEffect(() => {
+		let slider = setInterval(()=> {
+			setIndex(index + 1);
+		}, 7000);
+		return () => clearInterval(slider);
+	}, [index]);
+	
+	return (
+		<section className='section'>
+			<div className='title'>
+				<h3 className='sub-title'>Marvel</h3>
+				<h1>Avengers</h1>
+			</div>
+			<div className='section-center'>
+				{
+					superhero.map((hero, heroIndex) => {
+						const {id, img, name, desc} = hero;
+
+						let position = 'nextSlider';
+						if(heroIndex === index) {
+							position = 'activeSlide';
+						}
+						if(heroIndex === index - 1 || (index === 0 && heroIndex === superhero.length -1)) {
+							position = 'lastSlider';
+						}
+
+						return (
+							<article key={id} className={position}>
+								<img src={img} alt={name} className='hero-img'/>
+								<h4 className='hero-name'>{name}</h4>
+								<p className='text'>{desc}</p>
+							</article>
+						);
+					})
+				}
+				<button className='prev' onClick={()=> setIndex(index - 1)}>
+          <FiChevronLeft/>
+        </button>
+        <button className='next' onClick={()=> setIndex(index + 1)}>
+          <FiChevronRight/>
+        </button>
+			</div>
+		</section>
+	);
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,82 @@
+const catalog = [
+	{
+		id: 1,
+		name: 'Black Panther',
+		img: './images/black-panther.png',
+		desc: 'T’Challa is the king of the secretive and highly advanced African nation of Wakanda - as well as the powerful warrior known as the Black Panther'
+	},
+	{
+		id: 2,
+		name: 'Black Widow',
+		img: './images/black-widow.png',
+		desc: 'Despite super spy Natasha Romanoff’s checkered past, she’s become one of S.H.I.E.L.D.’s most deadly assassins and a frequent member of the Avengers'
+	},
+	{
+		id: 3,
+		name: 'Captain America',
+		img: './images/captain-america.png',
+		desc: 'Recipient of the Super-Soldier serum, World War II hero Steve Rogers fights for American ideals as one of the world’s mightiest heroes and the leader of the Avengers'
+	},
+	{
+		id: 4,
+		name: 'Doctor Strange',
+		img: './images/doctor-strange.png',
+		desc: 'Formerly a renowned surgeon, Doctor Stephen Strange now serves as the Sorcerer Supreme—Earth’s foremost protector against magical and mystical threats'
+	},
+	{
+		id: 5,
+		name: 'Falcon',
+		img: './images/falcon.png',
+		desc: 'Set with his signature mechanical wings, Sam Wilson teams up as an Avenger alongside his longtime partner Captain America'
+	},
+	{
+		id: 6,
+		name: 'Hawkeye',
+		img: './images/hawk-eye.png',
+		desc: 'A master marksman and longtime friend of Black Widow, Clint Barton serves as the Avengers’ amazing archer'
+	},
+	{
+		id: 7,
+		name: 'Hulk',
+		img: './images/hulk.png',
+		desc: 'Dr. Bruce Banner lives a life caught between the soft-spoken scientist he’s always been and the uncontrollable green monster powered by his rage'
+	},
+	{
+		id: 8,
+		name: 'Iron Man',
+		img: './images/iron-man.png',
+		desc: "Genius. Billionaire. Playboy. Philanthropist. Tony Stark's confidence is only matched by his high-flying abilities as the hero called Iron Man"
+	},
+	{
+		id: 9,
+		name: 'Spiderman',
+		img: './images/spiderman.png',
+		desc: "Bitten by a radioactive spider, Peter Parker’s arachnid abilities give him amazing powers he uses to help others, while his personal life continues to offer plenty of obstacles"
+	},
+	{
+		id: 10,
+		name: 'Thor',
+		img: './images/thor.png',
+		desc: "The son of Odin uses his mighty abilities as the God of Thunder to protect his home Asgard and planet Earth alike"
+	},
+	{
+		id: 11,
+		name: 'Vision',
+		img: './images/vision.png',
+		desc: "The android called Vision defies physics and fights as an Avenger with the power of density manipulation and his flawless computer brain"
+	},
+	{
+		id: 12,
+		name: 'Wanda',
+		img: './images/wanda-maximoff.png',
+		desc: "Notably powerful, Wanda Maximoff has fought both against and with the Avengers, attempting to hone her abilities and do what she believes is right to help the world"
+	},
+	{
+		id: 13,
+		name: 'War Machine',
+		img: './images/war-machine.png',
+		desc: 'Military veteran James Rhodes is ready for combat in his advanced armor, adding a formidable arsenal to Tony Stark-created designs'
+	},
+];
+
+export default catalog;
